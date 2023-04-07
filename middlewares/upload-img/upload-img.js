ecb821d855ebfbb1f6f6a9f53f3f51f219dcbd0f
KMeniawy <karim.elmeniawy.99@gmail.com>
2023-04-07 09:15:53 +0200

    img dimensions

--- middlewares/upload-img/upload-img.js
+++ middlewares/upload-img/upload-img.js
@@ -63,8 +63,8 @@ exports.sharpHandler = async (buffer, id) => {
   const uniqueNumber = Date.now();
   await sharp(buffer)
     .resize({
-      width: 600,
-      height: 400,
+      width: 2400,
+      height: 1600,
       fit: "contain",
       background: { r: 255, g: 255, b: 255, alpha: 1 },
     })
